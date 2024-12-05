fe5571f630e6f4329a609d387faa6535596660c2
PhoengZ <pholapcondo11@gmail.com>
2024-12-05 13:52:49 +0700

    test main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,25 @@
 package application;
 
-public class Main {
+import javafx.stage.Stage;
+import player.Player;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 
+
+public class Main extends Application{
+	private boolean isJump;
+	private Player player;
+	
+	public void start(Stage primaryStage)  throws Exception {
+		StackPane st = new StackPane();
+		Scene scene = new Scene(st,900,500);
+		primaryStage.setTitle("Collos");
+		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
+		primaryStage.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
